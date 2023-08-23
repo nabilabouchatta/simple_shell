755c8f26d9f041715233bcb7041ff18c296cfa8f
root <root@LAPTOP-0FFUURN6.localdomain>
2023-08-24 00:01:47 +0200

    ok

--- tokenize.c
+++ tokenize.c
@@ -1,56 +1,41 @@
 #include "shell.h"
-
 /**
- * tokenize_input -  tokenize an input line into individual
- * tokens based on a specified set of delimiters
- * @input_line : the line to be tolenized
- *
- * Return: return the command array
+ * tokenize - this function separate the string using a designed delimiter
+ * @data: a pointer to the program's data
+ * Return: an array of the different parts of the string
  */
-char **tokenize_input(char *input_line)
+void tokenize(data_of_program *data)
 {
-    char *token = NULL;
-    char *tmp = NULL;
-    char delimiter[] = "\t\n";
-    int count = 0, i = 0;
-    char **command_tokens = NULL;
+	char *delimiter = " \t";
+	int i, j, counter = 2, length;
 
-    if (!input_line)
-        return (NULL);
-    tmp = _strdup(input_line);
+	length = str_length(data->input_line);
+	if (length)
+	{
+		if (data->input_line[length - 1] == '\n')
+			data->input_line[length - 1] = '\0';
+	}
 
-    token = strtok(tmp, delimiter);
-    if (token == NULL)
-    {
-        free(input_line), input_line = NULL;
-        free(tmp), tmp = NULL;
-        return (NULL);
-    }
-    while (token)
-    {
-        count++;
-        token = strtok(NULL, delimiter);
-    }
+	for (i = 0; data->input_line[i]; i++)
+	{
+		for (j = 0; delimiter[j]; j++)
+		{
+			if (data->input_line[i] == delimiter[j])
+				counter++;
+		}
+	}
 
-    free(tmp);
-    tmp = NULL;
-    command_tokens = malloc(sizeof(char *) * (count + 1));
-    if (!command_tokens)
-    {
-        free(input_line);
-        input_line = NULL;
-        return (NULL);
-    }
-
-    token = strtok(input_line, delimiter);
-    while (token)
-    {
-        command_tokens[i] = token;
-        token = strtok(NULL, delimiter);
-        i++;
-    }
-    free(input_line);
-    input_line = NULL;
-    command_tokens[i] = NULL;
-    return (command_tokens);
+	data->tokens = malloc(counter * sizeof(char *));
+	if (data->tokens == NULL)
+	{
+		perror(data->program_name);
+		exit(errno);
+	}
+	i = 0;
+	data->tokens[i] = str_duplicate(_strtok(data->input_line, delimiter));
+	data->command_name = str_duplicate(data->tokens[0]);
+	while (data->tokens[i++])
+	{
+		data->tokens[i] = str_duplicate(_strtok(NULL, delimiter));
+	}
 }
